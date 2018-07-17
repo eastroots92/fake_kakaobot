26815e4be9551379c9d34c0c6fe2c116c143a572
Eastroots92 <eastroots92@gmail.com>
2018-07-17 23:02:07 +0900

    Setup Button & EditText Action in MainActivity
    버튼과 EditText에 대응하는 동작을 생성한다.
    
    Button 클릭 시  EditText의 값을 가져와 출력한다.

--- app/src/main/java/com/openull/eastroots92/fake_kakaobot/MainActivity.java
+++ app/src/main/java/com/openull/eastroots92/fake_kakaobot/MainActivity.java
@@ -2,12 +2,35 @@ package com.openull.eastroots92.fake_kakaobot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+  private EditText editText_input;
+  private Button button_sned;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    init();
+  }
+
+  private void init() {
+    editText_input = findViewById(R.id.editText_input);
+    sendButtonListener();
+  }
+
+  private void sendButtonListener() {
+    button_sned = findViewById(R.id.button_send);
+
+    button_sned.setOnClickListener( (__) -> {
+      String currentMessage = editText_input.getText().toString();
+
+      Toast.makeText(this, currentMessage, Toast.LENGTH_SHORT).show();
+    });
   }
 }
