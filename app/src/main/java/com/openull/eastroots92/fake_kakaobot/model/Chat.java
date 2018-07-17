e9d5ee50ac6b73c10819862c2c9896417198665f
Eastroots92 <eastroots92@gmail.com>
2018-07-17 23:11:10 +0900

    Make chat data class
    채팅 내용을 담을 data class를 만든다

---
+++ app/src/main/java/com/openull/eastroots92/fake_kakaobot/model/Chat.java
@@ -0,0 +1,13 @@
+package com.openull.eastroots92.fake_kakaobot.model;
+
+public class Chat {
+  String speech;
+
+  public String getSpeech() {
+    return speech;
+  }
+
+  public void setSpeech(String speech) {
+    this.speech = speech;
+  }
+}
--- app/src/main/java/com/openull/eastroots92/fake_kakaobot/ui/MainActivity.java
+++ app/src/main/java/com/openull/eastroots92/fake_kakaobot/ui/MainActivity.java
@@ -1,10 +1,12 @@
-package com.openull.eastroots92.fake_kakaobot;
+package com.openull.eastroots92.fake_kakaobot.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.openull.eastroots92.fake_kakaobot.R;
 
 public class MainActivity extends AppCompatActivity {
 
