07747b4ae7abf4065c9f0ae4daed419b35885e9b
Eastroots92 <eastroots92@gmail.com>
2018-07-17 23:24:14 +0900

    init RecyclerView for chatting layout
    Chatting Layout을 위한 RecycerView 설치

--- app/src/main/java/com/openull/eastroots92/fake_kakaobot/ui/MainActivity.java
+++ app/src/main/java/com/openull/eastroots92/fake_kakaobot/ui/MainActivity.java
@@ -2,6 +2,8 @@ package com.openull.eastroots92.fake_kakaobot.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
   private EditText editText_input;
   private Button button_sned;
   private List<Chat> chat;
+  private RecyclerView recyclerView_chatting;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
   private void init() {
     editText_input = findViewById(R.id.editText_input);
     sendButtonListener();
+    initChattingRecyclerView();
   }
 
   private void sendButtonListener() {
@@ -38,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
 
       Toast.makeText(this, currentMessage, Toast.LENGTH_SHORT).show();
     });
+  }
+
+  private void initChattingRecyclerView() {
+    recyclerView_chatting = findViewById(R.id.recyclerView_chatting);
+
+    recyclerView_chatting.setLayoutManager(new LinearLayoutManager(this));
   }
 
   private void appendChat(Chat chat) {
